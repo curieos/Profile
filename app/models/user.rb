e27effd5cde29996156e9e19a8b0444369d36d99
Caroline Longnecker <cvrieous@gmail.com>
2020-09-15 12:11:39 -0500

    Create user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
 class User < ApplicationRecord
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP, message: 'requires a valid email format' }, presence: true
+  validates :name, length: { minimum: 4, maximum: 20 }, presence: true
+  validates :role, presence: true
+  validate :valid_role
   has_secure_password
+
+  private
+
+  def valid_role
+    return if role == 'admin' || role == 'poster' || role == 'commenter'
+
+    errors.add(:role, 'is not valid')
+  end
 end
